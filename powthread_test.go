a67542b742592d9fe39cda43176168750464fd41
robertgass <robertgass@gmail.com>
2020-12-18 08:50:20 -0600

    Update tests, add travis yml

--- powthread_test.go
+++ powthread_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestThreadWork(t *testing.T) {
@@ -11,23 +12,28 @@ func TestThreadWork(t *testing.T) {
 	r := bytes.NewReader([]byte("This is a test.  This is only a test. 123"))
 	//                            0123456789012345
 	//min := Sha256MinWork{uint64(0x00000FFFFFFFFFFF)}
-	min := Sha256MinWork{uint64(0x0000001FFFFFFFF)}
-	fmt.Printf("%x\n", min)
-	d, w, er := ThreadWork(r, s, min, 10, nil)
-	if er != nil {
-		t.Error(er)
+	min := Sha256MinWork{uint64(0x0000FFFFFFFFFFFF)}
+	starttime := time.Now()
+	for i := 0; i < 10; i++ {
+		d, w, er := ThreadWork(r, s, min, 10, nil)
+		if er != nil {
+			t.Error(er)
+		}
+		if d == nil {
+			t.Errorf("digest is nil")
+		}
+		if w == nil {
+			t.Errorf("proof of work is nil")
+		}
+		v, er2 := s.Verify(d, min, w)
+		if er2 != nil {
+			t.Error(er2)
+		}
+		if !v {
+			t.Errorf("Work did not verify")
+		}
 	}
-	if d == nil {
-		t.Errorf("digest is nil")
-	}
-	if w == nil {
-		t.Errorf("proof of work is nil")
-	}
-	v, er2 := s.Verify(d, min, w)
-	if er2 != nil {
-		t.Error(er2)
-	}
-	if !v {
-		t.Errorf("Work did not verify")
-	}
+	donetime := time.Now()
+	worktime := donetime.Sub(starttime)
+	fmt.Printf("multithread time %s\n", worktime)
 }
--- sha256_test.go
+++ sha256_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestAllSha256(t *testing.T) {
@@ -12,20 +13,26 @@ func TestAllSha256(t *testing.T) {
 	d, _ := s.Digest(r)
 	//                            0123456789012345
 	//min := Sha256MinWork{uint64(0x00000FFFFFFFFFFF)}
-	min := Sha256MinWork{uint64(0x0FFFFFFFFFFFFFF)}
-	fmt.Printf("%x\n", min)
-	w, err := s.Work(d, min, nil)
-	if err != nil {
-		t.Error(err)
+	//min := Sha256MinWork{uint64(0x0FFFFFFFFFFFFFF)}
+	min := Sha256MinWork{uint64(0x0000FFFFFFFFFFFF)}
+	starttime := time.Now()
+	for i := 0; i < 10; i++ {
+		w, err := s.Work(d, min, nil)
+		if err != nil {
+			t.Error(err)
+		}
+		if w == nil {
+			t.Error("work is nil")
+		}
+		v, err2 := s.Verify(d, min, w)
+		if err2 != nil {
+			t.Error(err2)
+		}
+		if !v {
+			t.Errorf("Work did not verify")
+		}
 	}
-	if w == nil {
-		t.Error("work is nil")
-	}
-	v, err2 := s.Verify(d, min, w)
-	if err2 != nil {
-		t.Error(err2)
-	}
-	if !v {
-		t.Errorf("Work did not verify")
-	}
+	donetime := time.Now()
+	worktime := donetime.Sub(starttime)
+	fmt.Printf("single thread %s\n", worktime)
 }
